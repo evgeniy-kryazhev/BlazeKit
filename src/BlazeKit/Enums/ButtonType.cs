03b007d1248a58f9cb6dfbdf0657dcff1b98dfdd
evgeniy.kryazhev <evgeniy.kryazhev@actonica.ru>
2022-09-16 16:00:13 +0700

    create button component

---
+++ src/BlazeKit/Enums/ButtonType.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazeKit.Enums;
+
+public enum ButtonType
+{
+    [ClassName("is-primary")]
+    Primary,
+    [ClassName("is-danger")]
+    Danger,
+    [ClassName("is-info")]
+    Info
+}
